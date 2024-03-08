253994a0a0bf598444803cb08df6545c7220fb34
Abdelabset <Abdelbaset.Abidi@studmail.w-hs.de>
2024-03-08 14:48:04 +0100

    first commit, project file

---
+++ spring-cloud/src/test/java/com/abidi/springcloud/SpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abidi.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
